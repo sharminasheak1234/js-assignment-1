2baa38c6332309da4d1847f303056817908c371a
sharminasheak1234 <sharminasheak@gmail.com>
2026-09-03 21:39:48 +0600

    qs5

--- answer.js
+++ answer.js
@@ -1,3 +1,6 @@
+//qs-1
+
+
 function describeValue(value){
 
     if(value){
@@ -56,7 +59,7 @@ function validateUsername(username){
 function getCngFare(distance, isNight=false, waitingMinutes=0){
 let fare=50;
 
-if (distance>=2){
+if (distance>2){
     
     fare +=(distance-2)*15;
 
@@ -66,8 +69,42 @@ fare+=waitingMinutes*2;
 if(isNight){
     fare +=fare*.20;
 }
-return fare
+return fare;
 }
+
+
+//q-5
+
+const getChaseVerdict = (target, scored, ballsLeft)=>{
+    const runsNeeded = target - scored;
+
+    if(runsNeeded<=0){
+        return "Won";
+    }
+
+    if(ballsLeft<=0){
+         return "Lost";
+    }
+
+    const requiredRate = (runsNeeded / ballsLeft) * 6;
+
+    let verdict
+
+    if(requiredRate<=6){
+        verdict="Comfortable";
+    }
+    else  if(requiredRate<=12){
+        verdict="Tough";
+    }
+    else{
+    verdict="Almost Impossible";
+   } 
+
+ return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+
+}
+
+
 
 
 
