26507ab6647f069cce92a907363973a116dbf5ad
sharminasheak1234 <sharminasheak@gmail.com>
2026-09-03 19:22:09 +0600

    qs4

--- answer.js
+++ answer.js
@@ -50,5 +50,25 @@ function validateUsername(username){
      }
 }
 
+///q-4
+
+
+function getCngFare(distance, isNight=false, waitingMinutes=0){
+let fare=50;
+
+if (distance>=2){
+    
+    fare +=(distance-2)*15;
+
+}
+fare+=waitingMinutes*2;
+
+if(isNight){
+    fare +=fare*.20;
+}
+return fare
+}
+
+
 
 
